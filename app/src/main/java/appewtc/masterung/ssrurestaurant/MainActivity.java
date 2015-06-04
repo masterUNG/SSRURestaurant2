207e66e0586853127bab03c0439fe7f21f5e733c
masterUNG <phrombutr@gmail.com>
2015-06-04 14:00:05 +0700

    Syb

--- app/src/main/java/appewtc/masterung/ssrurestaurant/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ssrurestaurant/MainActivity.java
@@ -1,9 +1,24 @@
 package appewtc.masterung.ssrurestaurant;
 
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,9 +36,79 @@ public class MainActivity extends AppCompatActivity {
         objFoodTABLE = new FoodTABLE(this);
 
         //Test Add Value
-        testAddValue();
+        //testAddValue();
+
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
 
     }   // onCreate
+
+    private void synJSONtoSQLite() {
+
+        //Setup New Policy
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(myPolicy);
+        }
+
+        InputStream objInputStream = null;
+        String strJSON = "";
+
+        //1. Create InputStream
+        try {
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/ssru3/php_get_data_master.php");
+            HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+            HttpEntity objHttpEntity = objHttpResponse.getEntity();
+            objInputStream = objHttpEntity.getContent();
+
+        } catch (Exception e) {
+            Log.d("ssru", "InputStream ==> " + e.toString());
+        }
+
+
+        //2. Create strJSON
+        try {
+
+            BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+            StringBuilder objStringBuilder = new StringBuilder();
+            String strLine = null;
+
+            while ((strLine = objBufferedReader.readLine()) != null ) {
+                objStringBuilder.append(strLine);
+            }
+            objInputStream.close();
+            strJSON = objStringBuilder.toString();
+
+        } catch (Exception e) {
+            Log.d("ssru", "strJSON ==> " + e.toString());
+        }
+
+
+        //3. Update SQLite
+        try {
+
+            final JSONArray objJsonArray = new JSONArray(strJSON);
+
+            for (int i = 0; i < objJsonArray.length(); i++) {
+
+                JSONObject objJSONObject = objJsonArray.getJSONObject(i);
+                String strUser = objJSONObject.getString("User");
+                String strPassword = objJSONObject.getString("Password");
+                String strName = objJSONObject.getString("Name");
+
+                //update to SQLite
+                objUserTABLE.addNewUser(strUser, strPassword, strName);
+
+            }   // for
+
+        } catch (Exception e) {
+            Log.d("ssru", "Update ==> " + e.toString());
+        }
+
+
+    }   // synJSONtoSQLite
 
     private void testAddValue() {
 
